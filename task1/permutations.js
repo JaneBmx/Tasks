50ae626b3630b81485b325a701af7da61d823885
JaneBmx <49514807+JaneBmx@users.noreply.github.com>
2020-08-22 00:10:46 +0300

    Update permutations.js

--- task1/permutations.js
+++ task1/permutations.js
@@ -1,3 +1,3 @@
-let o=new Map,s=process.argv.slice(2),g=s=>s.split("").sort().join()
+o=new Map,s=process.argv.slice(2),g=s=>s.split("").sort().join()
 s.forEach(w=>o.set(g(w),1+o.get(g(w))||1))
 s.forEach(w=>g(w)==[...o].sort((a,b)=>b[1]-a[1])[0][0]&&console.log(w))
